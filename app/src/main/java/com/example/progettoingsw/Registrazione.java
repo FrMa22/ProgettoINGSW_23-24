ec068ade61370825be121cb982eb9b56c91380df
mark0 <mark04.mdl@gmail.com>
2023-11-10 14:55:29 +0100

    schermata registrazione e pop up

---
+++ app/src/main/java/com/example/progettoingsw/Registrazione.java
@@ -0,0 +1,38 @@
+package com.example.progettoingsw;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Spinner;
+import android.widget.TextView;
+public class Registrazione extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrazione);
+
+        MaterialButton bottoneAnnulla = (MaterialButton) findViewById(R.id.bottoneAnnulla);
+        MaterialButton bottoneProsegui = (MaterialButton) findViewById(R.id.bottoneProsegui);
+
+        bottoneAnnulla.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(String.valueOf(MainActivity.class));
+                startActivity(intent);
+
+            }
+        });
+
+      /*  bottoneProsegui.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(Registrazione.this,ClasseDopo.class);
+                startActivity(intent);
+
+            }
+        }); */
+    }
+}
