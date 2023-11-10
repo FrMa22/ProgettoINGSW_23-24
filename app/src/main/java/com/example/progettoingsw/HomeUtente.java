e63130edb430c5e88339e2032825edfe0d0f8fff
FrMa22 <francesco.magri35@gmail.com>
2023-11-10 10:23:24 +0100

    Aggiunto alla home: una toolbar non funzionante creata solo per scelte grafiche successive, le scrollview con elementi che permettono di avere diversi oggetti uno dopo l'altro da visualizzare scorrendo verso destra, aggiunto anche lo style per avere gli EditText (o altro) con bordi curvi.

---
+++ app/src/main/java/com/example/progettoingsw/HomeUtente.java
@@ -0,0 +1,27 @@
+package com.example.progettoingsw;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.button.MaterialButton;
+
+public class HomeUtente extends AppCompatActivity{
+
+    DrawerLayout drawerLayout;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_utente);
+
+        }
+
+
+
+
+}
