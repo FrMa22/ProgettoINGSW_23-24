4ffe6f34bb7db9787ea526d1da88308e8e1da978
FrMa22 <francesco.magri35@gmail.com>
2024-01-24 16:23:00 +0100

    Modificato il funzionamento del bottom navigation drawer nella home e cambiate le relative classi/file XML da activity a fragment

---
+++ app/src/main/java/com/example/progettoingsw/ElencoCategorie.java
@@ -0,0 +1,38 @@
+package com.example.progettoingsw;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ElencoCategorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elenco_categorie);
+
+        /*ImageButton imageButtonArte = findViewById(R.id.ImageButtonArte);
+        ImageButton imageButtonAutomobili = findViewById(R.id.ImageButtonAutomobili);
+
+        imageButtonArte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Azioni da eseguire quando viene cliccato ImageButtonArte
+                Toast.makeText(ElencoCategorie.this, "Categoria Arte selezionata", Toast.LENGTH_SHORT).show();
+                // Aggiungi qui la logica per gestire il clic sull'ImageButton dell'arte
+            }
+        });
+
+        imageButtonAutomobili.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Azioni da eseguire quando viene cliccato ImageButtonAutomobili
+                Toast.makeText(ElencoCategorie.this, "Categoria Automobili selezionata", Toast.LENGTH_SHORT).show();
+                // Aggiungi qui la logica per gestire il clic sull'ImageButton delle automobili
+            }
+        });*/
+    }
+}
---
+++ app/src/main/java/com/example/progettoingsw/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.progettoingsw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        // Impostazione del Fragment iniziale
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new FragmentHomeUtente())
+                .commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment;
+
+            // Imposta il fragment di default (potrebbe essere il fragment corrente)
+            Fragment currentFragment =(Fragment) getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+            selectedFragment = (currentFragment != null) ? currentFragment : new FragmentHomeUtente();
+
+            if (item.getItemId() == R.id.action_home) {
+                Log.d("BottomNav", "Selected Home");
+                selectedFragment = new FragmentHomeUtente();
+            } else if (item.getItemId() == R.id.action_categories) {
+                Log.d("BottomNav", "Selected Categories");
+                selectedFragment = new FragmentSelezioneCategorie();
+            } else if (item.getItemId() == R.id.action_crea_asta) {
+                Log.d("BottomNav", "Selected Crea Asta");
+                selectedFragment = new FragmentCreaLaTuaAstaAcquirente();
+            } else if (item.getItemId() == R.id.action_search) {
+                Log.d("BottomNav", "Selected Search");
+                selectedFragment = new FragmentRicercaAsta();
+            } else if (item.getItemId() == R.id.action_profile) {
+                Log.d("BottomNav", "Selected Profile");
+                selectedFragment = new FragmentProfilo();
+            }
+
+            // Controlla se il fragment corrente è già quello selezionato
+            if (currentFragment != null && currentFragment.getClass().equals(selectedFragment.getClass())) {
+                // Non fare nulla se il fragment corrente è già quello selezionato
+                Log.d("BottomNav", "Fragment already selected");
+                return true;
+            }
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+
+            return true;
+        });
+    }
+}
