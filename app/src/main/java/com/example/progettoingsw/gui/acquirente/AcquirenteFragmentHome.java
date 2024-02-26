4b8e4671e69480b93d057192d6da6c098d011419
FrMa22 <francesco.magri35@gmail.com>
2024-02-26 17:42:40 +0100

    Aggiunta GUI per venditore accessibile dal pop up iniziale cliccando su 'venditore'

---
+++ app/src/main/java/com/example/progettoingsw/gui/acquirente/AcquirenteFragmentHome.java
@@ -0,0 +1,44 @@
+package com.example.progettoingsw.gui.acquirente;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.progettoingsw.R;
+import com.example.progettoingsw.controllers_package.Controller;
+import com.example.progettoingsw.gui.PreferitiActivity;
+
+public class AcquirenteFragmentHome extends Fragment {
+
+    public AcquirenteFragmentHome() {
+        // Costruttore vuoto richiesto dal framework
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.acquirente_fragment_home, container, false);
+        // Il codice di AcquirenteFragmentHome è stato spostato qui
+        // Assicurati di adattare le referenze alle view
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ImageButton preferitiButton = view.findViewById(R.id.openPreferiti);
+        preferitiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Controller.redirectActivity(getActivity(), PreferitiActivity.class);
+            }
+        });
+
+    }
+}
