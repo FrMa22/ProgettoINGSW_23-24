1905919766bd3f3d71aa01e3e096e0f27436e0ae
EmmanuelZiviello <emmanuelziviello@gmail.com>
2024-04-02 18:24:24 +0200

    acquistaAstaTest

---
+++ app/src/test/java/com/example/progettoingsw/DAO/AcquistaAstaTest.java
@@ -0,0 +1,14 @@
+package com.example.progettoingsw.DAO;
+
+import static org.junit.Assert.*;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+public class AcquistaAstaTest extends TestCase {
+
+    @Test
+    public void acquistaAsta() {
+    }
+}
